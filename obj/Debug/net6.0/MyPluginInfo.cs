745ae4a031b12d3ceec4e4bf4b70cf83e540af57
akebono-1382 <tiramisu334@outlook.com>
2026-09-04 17:35:28 +0900

    objも削除

--- obj/Debug/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace HyperRoles
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "HyperRoles";
-        public const string PLUGIN_NAME = "My first plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
